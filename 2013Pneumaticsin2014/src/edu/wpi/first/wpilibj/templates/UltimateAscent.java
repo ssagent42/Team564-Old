131df16b6593104cfe27634c6996f101552fc189
ssagent42 <sukhjindersingh564@gmail.com>
2014-01-11 15:58:35 -0500

    contructed objects and stuf

--- 2013Pneumaticsin2014/src/edu/wpi/first/wpilibj/templates/UltimateAscent.java
+++ 2013Pneumaticsin2014/src/edu/wpi/first/wpilibj/templates/UltimateAscent.java
@@ -8,7 +8,23 @@
 package edu.wpi.first.wpilibj.templates;
 
 
+import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.image.CriteriaCollection;
+import edu.wpi.first.wpilibj.image.NIVision;
+import edu.wpi.first.wpilibj.ADXL345_I2C;
+import edu.wpi.first.wpilibj.Accelerometer;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,6 +38,27 @@ public class UltimateAscent extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+    
+    DoubleSolenoid frontarm; //Controls arm in front, horizontal motion
+    DoubleSolenoid toparm; //top arm big pistons
+    Joystick left;
+    Joystick right;
+    Joystick arc;
+    Compressor comp; //compressor
+    Jaguar jag1; //driving jags
+    Jaguar jag2;
+    Jaguar jag3; //winch jags
+    Jaguar jag4;
+    
+    
+    public UltimateAscent(){
+        
+        frontarm = new DoubleSolenoid();  
+        toparm = new DoubleSolenoid();
+        
+        
+    }
+    
     public void robotInit() {
 
     }
